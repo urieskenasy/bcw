32d73f504b30c449c9b8adfdeb2eac0e181e987c
urieskenasy <urieskenasy@gmail.com>
2022-08-17 23:22:36 +0200

    team and values updated

---
+++ src/components/pages/teamAndValues/team/teamData.js
@@ -0,0 +1,33 @@
+import imageRAN from "./Profile_Images/Ran_Shoshan_Profile_Picture.png";
+import imageEILON from "./Profile_Images/Eilon_Cohen_Profile_Picture.png";
+import imageYOHAI from "./Profile_Images/Yohai_Sibon_Profile_Picture.png";
+import imageROY from "./Profile_Images/Roy_Livne_Profile_Picture.png";
+const team = [
+    {
+        num: 1,
+        name: "Ran Shoshan – CEO",
+        description: `Ran, a Serial entrepreneur with experience in setting up and managing businesses in a variety of different sectors and managing dozens of employees and large budgets, has founded a startup company.\n
+    Has extensive and proven experience in product management, business development, networking, and strategic business planning.`,
+        pictureUrl: imageRAN,
+    },
+    {
+        num: 2,
+        name: "Roy Livne – CTO & Head of 3D",
+        description: `Roy, programmer, 3D artist/generalist. Experienced with unreal engine, 3D pipeline, creating characters/props/assets, game prototyping, and making ideas come to life.`,
+        pictureUrl: imageROY,
+    },
+    {
+        num: 3,
+        name: "Yohai Sitbon – Head of Business Development",
+        description: `Yohai, an entrepreneur with a proven track record, a planner, and a doer. Finishing up a degree in business administration (specialization in digital innovation) at Reichman University, Israel. Has been in the crypto world since 2016 and has a wide network of contacts across the space.`,
+        pictureUrl: imageYOHAI,
+    },
+    {
+        num: 4,
+        name: "Eilon Cohen – Head of Crypto",
+        description: `Eilon, Based in Tel Aviv, software & crypto engineer, cyber security expert, and digital technologies explorer. Mastered the Ethereum network developing environment, Web development, and Security research.`,
+        pictureUrl: imageEILON,
+    },
+];
+
+export default team;
